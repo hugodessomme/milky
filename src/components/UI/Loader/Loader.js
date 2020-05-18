626499bc8c4c9a8d4717e81987c04b71d310739e
hugodessomme <hdessomme@gmail.com>
2020-05-18 09:51:34 +0200

    Add loader component

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import css from './Loader.module.css';
+
+export const loader = () => <div className={css.loader}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>;
+
+export default loader;
